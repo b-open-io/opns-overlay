16d2f73ab7ef62fde0ec93b83f5f29934676d2ee
David Case <david.case@shruggr.cloud>
2025-04-12 21:42:40 +0000

    update paymail

--- cmd/paymail/paymail.go
+++ cmd/paymail/paymail.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/bitcoin-sv/go-paymail/logging"
 	"github.com/bitcoin-sv/go-paymail/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load("../../.env")
 	logger := logging.GetDefaultLogger()
 
 	sl := server.PaymailServiceLocator{}
@@ -17,13 +22,17 @@ func main() {
 	sl.RegisterPikePaymentService(new(OpnsServiceProvider))
 
 	var err error
-	port := 3001
-	// portEnv := os.Getenv("PORT")
-	// if portEnv != "" {
-	// 	if port, err = strconv.Atoi(portEnv); err != nil {
-	// 		logger.Fatal().Msg(err.Error())
-	// 	}
-	// }
+	PORT, _ := strconv.Atoi(os.Getenv("PAYMAIL_PORT"))
+	flag.IntVar(&PORT, "p", PORT, "Port to listen on")
+	flag.Parse()
+	if PORT == 0 {
+		PORT = 3001
+	}
+
+	paymailDomain := os.Getenv("PAYMAIL_DOMAIN")
+	if paymailDomain == "" {
+		paymailDomain = fmt.Sprintf("localhost:%d", PORT)
+	}
 	// Custom server with lots of customizable goodies
 	config, err := server.NewConfig(
 		&sl,
@@ -31,10 +40,10 @@ func main() {
 		server.WithP2PCapabilities(),
 		server.WithBeefCapabilities(),
 		// server.WithDomain("1sat.app"),
-		server.WithDomain(os.Getenv("PAYMAIL_DOMAIN")),
+		server.WithDomain(paymailDomain),
 		// server.WithDomain("localhost:3000"),
 		// server.WithGenericCapabilities(),
-		server.WithPort(port),
+		server.WithPort(PORT),
 		// server.WithServiceName("BsvAliasCustom"),
 		server.WithTimeout(15*time.Second),
 		// server.WithCapabilities(customCapabilities()),
